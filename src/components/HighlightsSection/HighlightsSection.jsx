2c5f37468172e0ee4e0df862cf61e98db04cdee6
Priyank Thakur <146356989+Priyank118@users.noreply.github.com>
2025-06-21 16:11:36 +0530

    Update HighlightsSection.jsx

--- src/components/HighlightsSection/HighlightsSection.jsx
+++ src/components/HighlightsSection/HighlightsSection.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Section from '../Section/Section';
-// We no longer need the useScrollReveal hook in this component
-// import useScrollReveal from '../../hooks/useScrollReveal';
+import useScrollReveal from '../../hooks/useScrollReveal';
 
 const HighlightsSection = ({ onNavLinkClick }) => {
     const highlightsData = [
@@ -9,24 +8,21 @@ const HighlightsSection = ({ onNavLinkClick }) => {
         { icon: '/images/airdrop.png', alt: 'BGMI Airdrop Icon', title: 'Custom Lobbies', description: 'Join our custom BGMI lobbies for daily action, map rotations, and community games. Perfect for squads and solo players.', buttonText: 'Coming Soon', buttonLink: '#' },
         { icon: '/images/chicken.png', alt: 'BGMI Chicken Dinner Icon', title: 'Organize Your Event', description: 'We provide end-to-end solutions from registrations, server hosting, live streaming, to prize distribution.', buttonText: 'Host With Us', buttonLink: 'https://docs.google.com/forms/d/e/1FAIpQLSfBiqQ4n7pzzfy7LOkOFRXFlv9xrOKXiZJPgFE45G9xx5OWbw/viewform?usp=header' },
     ];
-    // We also remove the instance of the hook
-    // const { observeElement } = useScrollReveal(0.05);
+    const { observeElement } = useScrollReveal(0.05);
 
     return (
         <Section id="highlights" title="BGMI Tournaments & Features">
             <div className="card-grid">
                 {highlightsData.map((item, index) => (
-                    // --- THESE ARE THE CHANGES ---
-                    // ref, className, and style for animation are removed from this div
                     <div
                         key={index}
-                        className="card"
+                        
+                        style={{ transitionDelay: `${index * 100}ms` }}
                     >
                         <img
                             src={item.icon}
                             alt={item.alt}
                             className="icon"
-                            // I've adjusted the icon size back to a more standard size
                             style={{height: '60px', width: '60px', margin: '0 auto 25px auto'}}
                             onError={(e) => { e.target.onerror = null; e.target.src = `https://placehold.co/60x60/242424/00aaff?text=ICON${index + 1}&font=roboto`; }}
                         />
